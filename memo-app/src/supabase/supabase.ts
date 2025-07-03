6b6e672b1234f8fd9f82c6ca8a308bb3af90cd3c
heroson02 <heroson0925@gmail.com>
2025-07-03 12:48:44 +0900

    remove private data

---
+++ memo-app/src/supabase/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseURL = 'https://kdwuvrblqggiojkvqfcu.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtkd3V2cmJscWdnaW9qa3ZxZmN1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE1MTE4ODQsImV4cCI6MjA2NzA4Nzg4NH0.lpcDJnfV0pohSBHIbqg3IfzlND9JqyEXxDto6i5zrPU'
+
+export const supabase = createClient(supabaseURL, supabaseKey);
